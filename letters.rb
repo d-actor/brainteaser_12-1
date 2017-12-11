c5bad1fbf4ca6f5f3a1b9355ea8f8d3c7386b232
Dan Actor <danltactor@gmail.com>
2017-12-11 10:25:46 -0700

    almost there

--- letters.rb
+++ letters.rb
@@ -7,18 +7,20 @@ def menu
   print `clear`
   puts "---Letters---".yellow
   puts "Enter a sentence and I will tell you which word has the most letters:".yellow
-  @entry = STDIN.gets.strip
+  @entry = STDIN.gets.strip.to_s
   letter_check
 end
 
 def letter_check
   words = @entry.split(/\W+/)
   words.each do |word|
-    letters = word.split("")
-    letters.each do |l|
-      
+    letters = word.split("").length
+    letters_uniq = word.split("").uniq.length
+    if letters != letters_uniq
+      @most << word
     end
   end
+  puts @most + " has the most letters."
 end
 
 menu
